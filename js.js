4e29be491c720145b3fccd7c5e35a05bc52b7ff1
Natalia Cicchino <nataliairene1986@gmail.com>
2021-10-28 15:13:38 -0300

    Clase 1

---
+++ js.js
@@ -0,0 +1,20 @@
+/* práctica primeros ejercicios */
+
+let nombreUsuario
+
+nombreUsuario = "jose"
+nombreUsuario = "Rubén"
+
+console.log ( nombreUsuario);
+
+/*let piedraPapelTijera = prompt( "Elige piedra, papel o tijera");
+console.log( piedraPapelTijera);*/
+let edadUsuario = prompt( "¿Qué edad tenés?");
+console.log(edadUsuario); 
+let multi =(edadUsuario * 365) ;
+console.log("viviste" + " " + multi + " " + "días" );
+
+/*let edad
+edad = 30;
+console.log(edad);*/
+
